47a1267e5432ec8e9418b345c13e15d7df84c292
rsf54 <ryfyguy@hotmail.com>
2023-01-25 13:29:15 -0500

    More Troop Anim
    Added textures and mats to troop models
    Setup animation transitions for idle and running
    Changed icon for selected/unselected troops

--- Assets/Scripts/Pathfinding/MovePlayerNew.cs
+++ Assets/Scripts/Pathfinding/MovePlayerNew.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MovePlayerNew : MonoBehaviour
 {
     public NavMeshAgent agent;
-
-    private void Start()
-    {
-        
-    }
+    public Animator animator;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
-    
+
+    private void Update()
+    {
+        
+        if (agent.remainingDistance <= 0.5)
+        {
+           animator.SetBool("IsRunning", false);
+        }
+
+    }
+
     public void MoveToDestination(Vector3 destination)
     {
+        animator.SetBool("IsRunning", true);
         agent.SetDestination(destination);
+
     }
 }
