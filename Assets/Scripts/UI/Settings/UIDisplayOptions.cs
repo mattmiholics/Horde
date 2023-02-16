b2c0037e3213e0eef68c20b021a704cc19fd6a63
rsf54 <ryfyguy@hotmail.com>
2023-02-16 15:44:17 -0500

    Display Settings And AO fix
    Added Display Settings and fixed Ambient Occlusion

--- Assets/Scripts/UI/Settings/UIDisplayOptions.cs
+++ Assets/Scripts/UI/Settings/UIDisplayOptions.cs
@@ -1,37 +1,111 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIDisplayOptions : MonoBehaviour
 {
-    public TMPro.TMP_Dropdown resolutionDropdown;
-    public TMPro.TMP_Dropdown qualityDropdown;
-    public TMPro.TMP_Dropdown textureDropdown;
+    [SerializeField] TMPro.TMP_Dropdown resolutionDropdown;
+    [SerializeField] TMPro.TMP_Dropdown displayMonitor;
+    [SerializeField] TMPro.TMP_Dropdown refreshRates;
+    [SerializeField] Toggle fullscreen;
+    [SerializeField] TMPro.TMP_Dropdown upscaling;
+    [SerializeField] Slider frameCap;
+    [SerializeField] Toggle vsync;
 
     Resolution[] resolutions;
+    Display[] displays;
+
+    const string RESOLUTION = "ResolutionPreference";
+    const string FULLSCREEN = "FullscreenPreference";
+    const string DISPLAY_MONITOR = "DisplayMonitor";
+    const string UPSCALING = "Upscaling";
+    const string REFRESH_RATE_PREF = "RefreshRate";
+    const string VSYNC = "VsyncPref";
+    const string FRAME_LIMIT = "FramerateCap";
+    int[] REFRESH_RATES = { 60, 75, 144, 240 };
 
     // Start is called before the first frame update
     void Start()
     {
+        findDisplays();
+        int currentResolutionIndex = findResolutions();
+        findRefreshRates(Screen.currentResolution.refreshRate);
+        LoadSettings(currentResolutionIndex);
+    }
+
+    void findDisplays()
+    {
+        displayMonitor.ClearOptions();
+
+        List<string> displayOptions = new List<string>();
+        displays = Display.displays;
+        int currentDisplayIndex = 0;
+        for (int i = 0; i < displays.Length; i++)
+        {
+            string option = "Display #" + (i+1);
+            displayOptions.Add(option);
+            if (displays[i] == Display.main)
+            {
+                currentDisplayIndex = i;
+            }
+        }
+        displayMonitor.AddOptions(displayOptions);
+        displayMonitor.RefreshShownValue();
+    }
+
+    int findResolutions()
+    {
         resolutionDropdown.ClearOptions();
-        List<string> options = new List<string>();
+        List<string> resOptions = new List<string>();
         resolutions = Screen.resolutions;
         int currentResolutionIndex = 0;
 
-        for (int i=0; i < resolutions.Length; i++) {
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            resOptions.Add(option);
             if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
         }
 
-        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.AddOptions(resOptions);
         resolutionDropdown.RefreshShownValue();
-        LoadSettings(currentResolutionIndex);
+
+        return currentResolutionIndex;
+    }
+
+    void findRefreshRates(int maxRefreshRate)
+    {
+        refreshRates.ClearOptions();
+        List<string> refreshOptions = new List<string>();
+        if (maxRefreshRate >= 240)
+        {
+            refreshOptions.Add("60hz");
+            refreshOptions.Add("75hz");
+            refreshOptions.Add("144hz");
+            refreshOptions.Add("240hz");
+        }
+        else if (maxRefreshRate >= 144)
+        {
+            refreshOptions.Add("60hz");
+            refreshOptions.Add("75hz");
+            refreshOptions.Add("144hz");
+        }
+        else if (maxRefreshRate >= 75)
+        { 
+            refreshOptions.Add("60hz");
+            refreshOptions.Add("75hz");
+        }
+        else
+        {
+            refreshOptions.Add("60hz");
+        }
+
+        refreshRates.AddOptions(refreshOptions);
+        refreshRates.RefreshShownValue();
     }
 
     public void SetFullscreen(bool isFullscreen)
@@ -43,72 +117,77 @@ public class UIDisplayOptions : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        findRefreshRates(resolution.refreshRate);
     }
 
-    public void SetTextureQuality(int textureIndex)
+    public void SetRefreshRate(int refreshIndex)
     {
-        QualitySettings.masterTextureLimit = textureIndex;
-        qualityDropdown.value = 6;
+        Resolution resolution = Screen.currentResolution;
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, REFRESH_RATES[refreshIndex]);
     }
 
-    public void SetQuality(int qualityIndex)
+    public void SetDisplay(int displayIndex)
     {
-        if (qualityIndex != 6)
-        {
-            QualitySettings.SetQualityLevel(qualityIndex);
-        }
-        switch (qualityIndex)
-        {
-            case 0:
-                textureDropdown.value = 3;
-                break;
-            case 1:
-                textureDropdown.value = 2;
-                break;
-            case 2:
-                textureDropdown.value = 1;
-                break;
-            case 3:
-                textureDropdown.value = 0;
-                break;
-        }
-        qualityDropdown.value = qualityIndex;
+        Display.displays[displayIndex].Activate();
     }
 
+    public void SetVsync(bool vsync)
+    {
+        if (vsync)
+            QualitySettings.vSyncCount = 1;
+        else
+            QualitySettings.vSyncCount = 0;
+    }
+
+    public void SetFrameLimit(float frameLimit)
+    {
+        Application.targetFrameRate = (int)frameLimit;
+    }
+    
     public void SaveSettings()
     {
-        PlayerPrefs.SetInt("QualitySettingPreference",qualityDropdown.value);
-        PlayerPrefs.SetInt("ResolutionPreference",resolutionDropdown.value);
-        PlayerPrefs.SetInt("TextureQualityPreference", textureDropdown.value);
-        PlayerPrefs.SetInt("FullscreenPreference", Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetInt(RESOLUTION, resolutionDropdown.value);
+        PlayerPrefs.SetInt(FULLSCREEN, Convert.ToInt32(fullscreen.isOn));
+        PlayerPrefs.SetInt(DISPLAY_MONITOR, displayMonitor.value);
+        PlayerPrefs.SetInt(REFRESH_RATE_PREF, refreshRates.value);
+        PlayerPrefs.SetInt(VSYNC, Convert.ToInt32(vsync.isOn));
+        PlayerPrefs.SetInt(FRAME_LIMIT, (int)frameCap.value);
     }
     
 
     public void LoadSettings(int currentResolutionIndex)
     {
-        if (PlayerPrefs.HasKey("QualitySettingPreference"))
-            qualityDropdown.value =
-                         PlayerPrefs.GetInt("QualitySettingPreference");
-        else
-            qualityDropdown.value = 3;
 
-        if (PlayerPrefs.HasKey("ResolutionPreference"))
-            resolutionDropdown.value =
-                         PlayerPrefs.GetInt("ResolutionPreference");
+        if (PlayerPrefs.HasKey(RESOLUTION))
+            resolutionDropdown.value = PlayerPrefs.GetInt(RESOLUTION);
         else
             resolutionDropdown.value = currentResolutionIndex;
 
-        if (PlayerPrefs.HasKey("TextureQualityPreference"))
-            textureDropdown.value =
-                         PlayerPrefs.GetInt("TextureQualityPreference");
+        if (PlayerPrefs.HasKey(FULLSCREEN))
+        {
+            Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt(FULLSCREEN));
+            fullscreen.isOn = Convert.ToBoolean(PlayerPrefs.GetInt(FULLSCREEN));
+        }
         else
-            textureDropdown.value = 0;
-
-        if (PlayerPrefs.HasKey("FullscreenPreference"))
-            Screen.fullScreen =
-            Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
-        else
+        {
             Screen.fullScreen = true;
+            fullscreen.isOn = true;
+        }
+
+        if (PlayerPrefs.HasKey(REFRESH_RATE_PREF))
+            refreshRates.value = PlayerPrefs.GetInt(REFRESH_RATE_PREF);
+        else
+            refreshRates.value = 0;
+
+        if (PlayerPrefs.HasKey(VSYNC))
+            vsync.isOn = Convert.ToBoolean(PlayerPrefs.GetInt(VSYNC));
+        else
+            vsync.isOn = false;
+
+        if (PlayerPrefs.HasKey(FRAME_LIMIT))
+            frameCap.value = PlayerPrefs.GetInt(FRAME_LIMIT);
+        else
+            frameCap.value = -1;
     }
 
     
--- Assets/Scripts/UI/Settings/UpdateSliderNum.cs
+++ Assets/Scripts/UI/Settings/UpdateSliderNum.cs
@@ -8,9 +8,18 @@ public class UpdateSliderNum : MonoBehaviour
     private void Awake()
     {
         UpdateText(GetComponentInParent<Slider>().value);
+        UpdateFrameRate(GetComponentInParent<Slider>().value);
     }
     public void UpdateText(float newVolume)
     {
         numberText.text = (Mathf.Round(newVolume*10)).ToString();
+    }
+
+    public void UpdateFrameRate(float newFrameRate)
+    {
+        if (newFrameRate != -1)
+            numberText.text = newFrameRate.ToString();
+        else
+            numberText.text = "No Limit";
     }
 }
