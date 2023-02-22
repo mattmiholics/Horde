9ebea9516c99407ee69449bb27c0167f2bd2ad85
rsf54 <ryfyguy@hotmail.com>
2023-02-22 16:01:33 -0500

    Visual Update for Audio Menu
    Switching from Legacy text to TMP

--- Assets/Scripts/UI/Settings/UpdateVolumeText.cs
+++ Assets/Scripts/UI/Settings/UpdateVolumeText.cs
@@ -1,11 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UpdateVolumeText : MonoBehaviour
 {
-    [SerializeField] Text volumeText;
+    [SerializeField] TextMeshProUGUI volumeText;
+    [SerializeField] Slider vol;
+    void Start()
+    {
+        UpdateText(vol.value);
+    }
     public void UpdateText(float newVolume)
     {
         volumeText.text = (Mathf.RoundToInt(newVolume)).ToString();
-    }
+    }    
 }
