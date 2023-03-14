f4f8ccc79a6435263b95f09b685a046df86e4e2b
SuperChrisBoy <lele2000213@gmail.com>
2023-03-14 18:10:34 -0400

    Fire mage with area damage

--- Assets/Scripts/Gameplay/Characters/Troops/Medieval/FireMage/FireMageTroopLogic.cs
+++ Assets/Scripts/Gameplay/Characters/Troops/Medieval/FireMage/FireMageTroopLogic.cs
@@ -6,25 +6,25 @@ using UnityEngine;
 
 public class FireMageTroopLogic : TroopData
 {
-    public override void Attack(UnitData unitData)
-    {
-        if (isRanged)
-        {
-            GameObject bulletParent = GameObject.Find("World/BulletParent");
-            GameObject bulletObj = (GameObject)Instantiate(bullet, firePoint.position, firePoint.rotation, bulletParent.transform);
-            FireMageBullet bulletS = bulletObj.GetComponent<FireMageBullet>();
+    // public override void Attack(UnitData unitData)
+    // {
+    //     if (isRanged)
+    //     {
+    //         GameObject bulletParent = GameObject.Find("World/BulletParent");
+    //         GameObject bulletObj = (GameObject)Instantiate(bullet, firePoint.position, firePoint.rotation, bulletParent.transform);
+    //         FireMageBullet bulletS = bulletObj.GetComponent<FireMageBullet>();
             
-            if(bulletS != null)
-            {
-                bulletS.Seek(unitData.transform, damage);
-            }
-        }
-        else
-        {
-            unitData.TakeDamage(damage);
-        }
-        Attacking.Invoke();
-        StartCoroutine(AttackCooldown(attackCooldown));
-    }
+    //         if(bulletS != null)
+    //         {
+    //             bulletS.Seek(unitData.transform, damage);
+    //         }
+    //     }
+    //     else
+    //     {
+    //         unitData.TakeDamage(damage);
+    //     }
+    //     Attacking.Invoke();
+    //     StartCoroutine(AttackCooldown(attackCooldown));
+    // }
 }
 
