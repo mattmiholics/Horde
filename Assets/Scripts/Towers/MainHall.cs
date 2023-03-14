106fe45463e94595759543b58afd07457ac88536
Unlockingice <jpo39@drexel.edu>
2023-03-14 18:20:18 -0400

    Main Hall Health Bar

--- Assets/Scripts/Towers/MainHall.cs
+++ Assets/Scripts/Towers/MainHall.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainHall : MonoBehaviour
 {
     public Transform target;
+
+    public Image healthBar;
+    public int maxHealth;
+
+    public GameObject gameManager;
+
+    public void Start()
+    {
+        maxHealth = gameManager.GetComponent<PlayerStats>().startLives;
+    }
+
+    public void Update()
+    {
+        healthBar.fillAmount = (float) gameManager.GetComponent<PlayerStats>().lives / maxHealth;
+       
+    }
+
+
 }
