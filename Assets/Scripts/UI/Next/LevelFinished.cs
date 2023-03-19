0c1f5da953eafc6cd6743f9e344e48658a471e56
rsf54 <ryfyguy@hotmail.com>
2023-03-19 16:34:26 -0400

    Fixed some Rules stuff and setup Level Finished Screen

---
+++ Assets/Scripts/UI/Next/LevelFinished.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class LevelFinished : MonoBehaviour
+{
+    public GameObject nextLevelButton;
+    [StringInList(typeof(PropertyDrawersHelper), "AllSceneNames")] public string mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel()
+    {
+        if (Application.CanStreamedLevelBeLoaded(LevelInfo.Instance.nextLevelString))
+            SceneLoader.Instance.LoadWorldScene(LevelInfo.Instance.nextLevelString, true);
+    }
+
+    public void Menu()
+    {
+        SceneLoader.Instance.LoadWorldScene(mainMenu, true);
+    }
+}
