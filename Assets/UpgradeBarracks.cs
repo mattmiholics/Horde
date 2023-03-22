08ab9bd5285ced3de56d7161666f3d8f0fde9197
maraudical <gradymilligan@icloud.com>
2023-03-22 15:42:30 -0400

    this caused build error

--- Assets/UpgradeBarracks.cs
+++ Assets/UpgradeBarracks.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Sirenix.OdinInspector.Editor.Drawers;
 
 public class UpgradeBarracks : MonoBehaviour
 {
