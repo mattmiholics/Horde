9e4f699a98393e1e6e0ed4b4ba8f3d167ed5736d
rsf54 <ryfyguy@hotmail.com>
2023-03-22 13:27:51 -0400

    fixed barracks stuff

--- Assets/UpgradeBarracks.cs
+++ Assets/UpgradeBarracks.cs
@@ -2,22 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Sirenix.OdinInspector.Editor.Drawers;
+
 public class UpgradeBarracks : MonoBehaviour
 {
     public GameObject upgradeText;
     public TowerData barracksData;
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-
-        if (barracksData.level == 3)
-        {
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            upgradeText.GetComponent<TMPro.TextMeshProUGUI>().text = "$" + barracksData.level * 1000;
-        }
+        upgradeText.GetComponent<TMPro.TextMeshProUGUI>().text = "$" + barracksData.level * 1000;
     }
 
     public void UpgradeBarracksButton()
@@ -25,6 +19,15 @@ public class UpgradeBarracks : MonoBehaviour
         if (PlayerStats.Instance.money >= barracksData.level * 1000 && barracksData.level < 3) {
             PlayerStats.Instance.money -= barracksData.level * 1000;
             barracksData.level++;
+            if (barracksData.level >= 3)
+            {
+                barracksData.isMaxLevel = true;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                upgradeText.GetComponent<TMPro.TextMeshProUGUI>().text = "$" + barracksData.level * 1000;
+            }
         }
     }
 }
